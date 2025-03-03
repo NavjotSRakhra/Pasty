b9d4d13baa577fbde4da6c6f48e8186cc2d4792f
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2025-03-03 22:37:41 +0530

    Initial commit

---
+++ src/main/java/io/github/navjotsrakhra/pasty/PastyApplication.java
@@ -0,0 +1,13 @@
+package io.github.navjotsrakhra.pasty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/navjotsrakhra/pasty/PastyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.navjotsrakhra.pasty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
